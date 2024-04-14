75b3ec198992706edff9050726a252d985d1fe61
Adi30TyaDTU <128904617+Adi30TyaDTU@users.noreply.github.com>
2024-04-14 15:05:01 +0000

    Experiments

--- Exp_02 : Write a program to implement Prims Algorithm.cpp
+++ Exp_02 : Write a program to implement Prims Algorithm.cpp
@@ -1,79 +1,63 @@
-// Prim's algorithm is a greedy algorithm used to find the minimum spanning tree (MST) of a connected, undirected graph. The algorithm starts with an arbitrary node and grows the MST by adding the shortest edge that connects the tree to a vertex not yet in the tree. This process is repeated until all vertices are included in the MST. 
 
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <utility>
-#include <limits>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-// Data structure to represent an edge
-struct Edge {
-    int src, dest, weight;
+class Solution
+{
+public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+	int spanningTree(int V, vector<vector<int>> adj[])
+	{
+		priority_queue<pair<int, int>,
+		               vector<pair<int, int> >, greater<pair<int, int>>> pq;
+
+		vector<int> vis(V, 0);
+		// {wt, node}
+		pq.push({0, 0});
+		int sum = 0;
+		while (!pq.empty()) {
+			auto it = pq.top();
+			pq.pop();
+			int node = it.second;
+			int wt = it.first;
+
+			if (vis[node] == 1) continue;
+			// add it to the mst
+			vis[node] = 1;
+			sum += wt;
+			for (auto it : adj[node]) {
+				int adjNode = it[0];
+				int edW = it[1];
+				if (!vis[adjNode]) {
+					pq.push({edW, adjNode});
+				}
+			}
+		}
+		return sum;
+	}
 };
 
-// Function to initialize and add an edge to the adjacency list
-void addEdge(vector<vector<pair<int, int>>>& adj, int u, int v, int weight) {
-    adj[u].emplace_back(v, weight);
-    adj[v].emplace_back(u, weight); // For undirected graph
-}
-
-vector<Edge> primMST(vector<vector<pair<int, int>>>& adj) {
-    int V = adj.size();
-    vector<bool> inMST(V, false);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    vector<Edge> MST;
-    vector<int> dist(V, numeric_limits<int>::max());
-    vector<int> parent(V, -1);
-
-    // Start with vertex 0
-    pq.push({0, 0}); // {distance, vertex}
-
-    while (!pq.empty()) {
-        int u = pq.top().second;
-        pq.pop();
-
-        inMST[u] = true;
-
-        for (const auto& edge : adj[u]) {
-            int v = edge.first;
-            int weight = edge.second;
-
-            if (!inMST[v] && weight < dist[v]) {
-                dist[v] = weight;
-                parent[v] = u;
-                pq.push({dist[v], v});
-            }
-        }
-    }
-
-    for (int i = 1; i < V; ++i) {
-        MST.push_back({parent[i], i, dist[i]});
-    }
-
-    return MST;
-}
 
 int main() {
-    int V = 5;
-    vector<vector<pair<int, int>>> adj(V);
 
-    // Adding edges to the graph
-    addEdge(adj, 0, 1, 2);
-    addEdge(adj, 0, 3, 6);
-    addEdge(adj, 1, 2, 3);
-    addEdge(adj, 1, 3, 8);
-    addEdge(adj, 1, 4, 5);
-    addEdge(adj, 2, 4, 7);
-    addEdge(adj, 3, 4, 9);
+	int V = 5;
+	vector<vector<int>> edges = {{0, 1, 2}, {0, 2, 1}, {1, 2, 1}, {2, 3, 2}, {3, 4, 1}, {4, 2, 2}};
+	vector<vector<int>> adj[V];
+	for (auto it : edges) {
+		vector<int> tmp(2);
+		tmp[0] = it[1];
+		tmp[1] = it[2];
+		adj[it[0]].push_back(tmp);
 
-    vector<Edge> MST = primMST(adj);
+		tmp[0] = it[0];
+		tmp[1] = it[2];
+		adj[it[1]].push_back(tmp);
+	}
 
-    cout << "Edges of Minimum Spanning Tree:\n";
-    for (const auto& e : MST) {
-        cout << e.src << " - " << e.dest << "  Weight: " << e.weight << "\n";
-    }
+	Solution obj;
+	int sum = obj.spanningTree(V, adj);
+	cout << "The sum of all the edge weights: " << sum << endl;
 
-    return 0;
+	return 0;
 }
+
